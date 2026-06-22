ccd858068b00527cf3a7534c560210048155a494
lekhnisrivastava09-collab <lekhnisrivastava09@gmail.com>
2026-06-22 17:27:00 +0530

    hard work
    took me 2 hours 7 minutes

---
+++ ppto.py
@@ -0,0 +1,34 @@
+
+numbers1 =(55.78,88.63,87.98,56.72,43.84,77.07)
+total_sum1 = sum(numbers1)
+print(f"The total  sum is: {total_sum1}")
+total_sum1 -= 2
+print(f"The total sum minus 2 is: {total_sum1}")
+total_sum1 *= 4
+print(f"The total multipilication by 4 is: {total_sumc}")
+total_sum1 /= 3
+print(f"The total division by 3 is: {total_sum1}")
+
+numbers2 =(22.09,87.98,56.88)
+total_sum2 = sum(numbers2)
+print(f"The total sum is: {total_sum2}")
+total_sum2 -= 2
+print(f"The total sum is: {total_sum2}")
+total_sum2 *=4
+print(f"The total multipilication by 4 is: {total_sum2}")
+total_sum2 /=3
+print(f"The total division by 3 is: {total_sum2}")
+
+numbers1
+q - 55.78
+w - 88.63
+e - 87.98
+r - 56.72
+t - 43.84
+y - 77.07
+numbers2
+u - 22.09
+i - 87.98
+o - 56.88
+print(f"The total sum is: {numbers1 + numbers2 = final_sum}")
+
